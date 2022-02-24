d2706efbcabac09619defb874518fc7d5ded6c8e
shresthanishcom <me.anish.shrestha@gmail.com>
2022-02-25 00:06:08 +0545

    on maintainance added

---
+++ src/Maintainance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Maintainance() {
+  return (
+    <div style={{ height: "100%", width: "40vw", margin: "auto" }}>
+      <img src="maintainance.gif" alt="maintainance gif" />
+    </div>
+  );
+}
+
+export default Maintainance;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Maintainance from "./Maintainance";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Maintainance />
     </Provider>
   </React.StrictMode>,
 
