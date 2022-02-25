e450ca6c332a58d5df11542cff8b8c4d34d2a2fa
shresthanishcom <me.anish.shrestha@gmail.com>
2022-02-25 10:21:59 +0545

    error fixed maintainance

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
 import "./App.css";
 import "./Containers/Components/Css/Introduction.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCoffee, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { fab } from "@fortawesome/free-brands-svg-icons";
+// import { faCoffee, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
-import Introduction from "./Containers/Components/Introduction";
-import Header from "./Containers/Components/Header";
-import Footer from "./Containers/Components/Footer";
-
-library.add(fab, faCheckSquare, faCoffee);
+// import Introduction from "./Containers/Components/Introduction";
+// import Header from "./Containers/Components/Header";
+// import Footer from "./Containers/Components/Footer";
+import Maintainance from "./Maintainance";
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Introduction} />
-        </Switch>
-        <Footer />
-      </Router>
+      <Maintainance />
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import Maintainance from "./Maintainance";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Maintainance />
-    </Provider>
+    <App />
   </React.StrictMode>,
 
   document.getElementById("root")
